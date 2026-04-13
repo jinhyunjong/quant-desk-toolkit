c355709630795e49cc0c55d5fe01315c60a1bd35
Hyun Jin <62405594+hyun-quant@users.noreply.github.com>
2026-04-13 16:29:38 -0400

    Delete rates-analytics-engine/src/bond.py

--- rates-analytics-engine/src/bond.py
+++
@@ -1,6 +0,0 @@
-def bond_price(face, coupon_rate, maturity, yield_rate, freq=1):
-    price = 0
-    for t in range(1, maturity * freq + 1):
-        price += (face * coupon_rate / freq) / ((1 + yield_rate / freq) ** t)
-    price += face / ((1 + yield_rate / freq) ** (maturity * freq))
-    return price
